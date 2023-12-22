a1a2db5da7674e80cc14b439afca9b276f971eee
Nils Hollenstein <144909884+nilshollenstein@users.noreply.github.com>
2023-12-22 11:56:47 +0000

    Chore: Logout is working

--- src/authentication.js
+++ src/authentication.js
@@ -25,7 +25,19 @@ router.get('/verify', function (req, res) {
 	if (req.session.email) {
 		return res.status(200).json({ email: req.session.email });
 	}
-
+	console.log('Error 401: Not logged in')
 	return res.status(401).json({ error: '401, Not logged in' });
 });
+
+router.delete('/logout', (req, res)=>{
+	if (req.session.email) {
+		// Reset Cookie
+		req.session.email = null;
+
+		return res.send('Logout successful').status(204);
+	}
+
+	return res.status(401).json({ error: 'Not logged in' });
+})
+
 module.exports = router;
