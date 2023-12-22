b3cee4943eef3503543a7f72bb938a14188d76d1
Nils Hollenstein <144909884+nilshollenstein@users.noreply.github.com>
2023-12-22 12:09:46 +0000

    Chore: forgotten formatted File

--- src/authentication.js
+++ src/authentication.js
@@ -1,43 +1,44 @@
+/* eslint-disable no-console */
 const express = require('express');
+
 const router = express.Router();
 
-
 const credentials = {
-	password: 'm295',
+  password: 'm295',
 };
 
-router.post('/login', (req, res)=>{
-	const email = req.body.email;
-	const password = req.body.password;
+router.post('/login', (req, res) => {
+  const { email } = req.body;
+  const { password } = req.body;
 
-	if(!email || !password){
-		console.log('Error 422, Email and Password required')
-		return res.status(422).json( {error: '422, Email and Password required'})
-	}
-	if (password === credentials.password){
-		req.session.email = email;
-		return res.status(201).json({ email: req.session.email });
-	}
-	console.log('Error 401: Not logged in')
-	return res.status(401).json({ error: '401, Not logged in' });
-})
-router.get('/verify', function (req, res) {
-	if (req.session.email) {
-		return res.status(200).json({ email: req.session.email });
-	}
-	console.log('Error 401: Not logged in')
-	return res.status(401).json({ error: '401, Not logged in' });
+  if (!email || !password) {
+    console.log('Error 422, Email and Password required');
+    return res.status(422).json({ error: '422, Email and Password required' });
+  }
+  if (password === credentials.password) {
+    req.session.email = email;
+    return res.status(201).json({ email: req.session.email });
+  }
+  console.log('Error 401: Not logged in');
+  return res.status(401).json({ error: '401, Not logged in' });
+});
+router.get('/verify', (req, res) => {
+  if (req.session.email) {
+    return res.status(200).json({ email: req.session.email });
+  }
+  console.log('Error 401: Not logged in');
+  return res.status(401).json({ error: '401, Not logged in' });
 });
 
-router.delete('/logout', (req, res)=>{
-	if (req.session.email) {
-		// Reset Cookie
-		req.session.email = null;
+router.delete('/logout', (req, res) => {
+  if (req.session.email) {
+    // Reset Cookie
+    req.session.email = null;
 
-		return res.send('Logout successful').status(204);
-	}
+    return res.send('Logout successful').status(204);
+  }
 
-	return res.status(401).json({ error: 'Not logged in' });
-})
+  return res.status(401).json({ error: 'Not logged in' });
+});
 
 module.exports = router;
